fa2b9f8977709fbc3b71423f3d9048a9c9273a54
Mahmoud-abu-zaid <v.i.p.omlat@gmail.com>
2025-11-09 08:50:10 +0200

    feat(font): add global Exo 2 font to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import "../assets/style/globals.css";
-import BackGroundVideo from "@/components/backgroundVideo";
+import Header from "@/components/Header";
+import BackGroundVideo from "@/components/VideoBackground";
+import { Exo_2 } from "next/font/google";
 
+const exo_2 = Exo_2({
+  weight: "400",
+});
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -8,8 +13,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="">
-        <BackGroundVideo>{children}</BackGroundVideo>
+      <body  className={exo_2.className}>
+        <BackGroundVideo>
+          <Header />
+          {children}
+        </BackGroundVideo>
       </body>
     </html>
   );
