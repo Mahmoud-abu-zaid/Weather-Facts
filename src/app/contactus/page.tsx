caba6db5c09387450bda80d03912cfc9df8699dd
Mahmoud-abu-zaid <v.i.p.omlat@gmail.com>
2025-11-12 21:12:51 +0200

    feat(pages): add 'Coming Soon' placeholder pages

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -1,3 +1,12 @@
 export default function contactus() {
-  return <div className="pt-26 text-center text-4xl text-white">Contact us</div>;
+  return (
+    <div className="pt-26 text-center text-4xl text-white">
+      <h2>Contact us</h2>
+      <div className="text-center py-20">
+        <p className="text-7xl py-3 ">🚧</p>
+        <h2 className="sm:text-4xl text-3xl py-2 ">Coming Soon</h2>
+        <p className="sm:text-2xl text-lg ">This page is under construction. Check back soon!</p>
+      </div>
+    </div>
+  );
 }
--- src/app/downloadApp/page.tsx
+++ src/app/downloadApp/page.tsx
@@ -1,3 +1,12 @@
 export default function DownloadApp() {
-  return <div className="pt-26 text-center text-4xl text-white">Download App</div>;
+  return (
+    <div className="pt-26 text-center text-4xl text-white">
+      <h2>Download App</h2>
+      <div className="text-center py-20">
+        <p className="text-7xl py-3 ">🚧</p>
+        <h2 className="sm:text-4xl text-3xl py-2 ">Coming Soon</h2>
+        <p className="sm:text-2xl text-lg ">This page is under construction. Check back soon!</p>
+      </div>
+    </div>
+  );
 }
