4c011b6e7d2a723b09d280ae6e4a344d76bf5fc5
Mahmoud-abu-zaid <v.i.p.omlat@gmail.com>
2025-11-14 11:13:19 +0200

    fix(video): update background video path to correct location

--- src/components/VideoBackground.tsx
+++ src/components/VideoBackground.tsx
@@ -1,25 +1,13 @@
+"use client";
+
 export default function BackGroundVideo({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative w-full h-screen">
-      <video
-        className="fixed top-0 left-0 w-full h-full object-cover z-0"
-        autoPlay
-        loop
-        muted
-        playsInline
-        preload="auto"
-        poster="../../img/poster.wepb"
-        style={{
-          imageRendering: "auto",
-          transform: "translateZ(0)",
-        }}
-      >
-        <source src="../../video/bg.webm" type="video/webm" />
-        <source src="../../video/bg.mp4" type="video/mp4" />
+    <div className="relative w-full h-full overflow-hidden">
+      <video className="fixed top-0 left-0 w-full h-full object-cover z-0 " autoPlay loop muted playsInline preload="auto" poster="/img/poster.webp">
+        <source src="/video/bg.webm" type="video/webm" />
       </video>
 
       <div className="fixed inset-0 bg-black/30 z-10 pointer-events-none" />
-
       <div className="relative z-20">{children}</div>
     </div>
   );
