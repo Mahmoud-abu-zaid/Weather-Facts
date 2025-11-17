d4030ab01d2bbb5f1acf1dd317341ada4dfcc455
Mahmoud-abu-zaid <v.i.p.omlat@gmail.com>
2025-11-17 19:08:59 +0200

    fix(navpar): Reduce the number of lines in the code and make it easier to maintain

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,18 @@
 "use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { FaXmark } from "react-icons/fa6";
 import { HiOutlineMenu } from "react-icons/hi";
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
-
+  const pathname = usePathname();
+  const navLinks = [
+    { path: "/", name: "Home" },
+    { path: "/downloadApp", name: "Download App" },
+    { path: "/contactus", name: "Contact Us" },
+  ];
   return (
     <header className="fixed top-0 left-0 w-full z-50 bg-[#1B262C]/70 backdrop-blur-md text-white shadow-md">
       <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-12">
@@ -15,38 +21,30 @@ export default function Header() {
         </Link>
 
         <nav className="hidden lg:flex gap-10 text-lg">
-          <Link href="/" className="hover:text-[#00ADB5] transition-colors">
-            Home
-          </Link>
-          <Link href="/downloadApp" className="hover:text-[#00ADB5] transition-colors">
-            Download App
-          </Link>
-          <Link href="/contactus" className="hover:text-[#00ADB5] transition-colors">
-            Contact Us
-          </Link>
+          {navLinks.map((link) => (
+            <Link key={link.path} href={link.path} className={`hidden lg:flex hover:text-[#00ADB5] transition-colors ${pathname === link.path ? "text-[#00ADB5]" : ""}`}>
+              {link.name}
+            </Link>
+          ))}
         </nav>
 
         <Link href="/register" className="hidden lg:inline-block bg-[#00ADB5] hover:bg-[#008C96] text-white px-4 py-2 rounded-xl text-sm font-medium transition">
           Sign Up
         </Link>
 
-        <button onClick={() => setMenuOpen(!menuOpen)} className="lg:hidden text-3xl focus:outline-none" aria-label="Toggle menu">
+        <button onClick={() => setMenuOpen(!menuOpen)} className="lg:hidden text-3xl focus:outline-none cursor-pointer" aria-label="Toggle menu">
           {menuOpen ? <FaXmark /> : <HiOutlineMenu />}
         </button>
       </div>
 
       {menuOpen && (
         <div className="lg:hidden bg-[#1B262C]/90 backdrop-blur-xl flex flex-col items-center pb-4 text-lg animate-slideDown">
-          <Link href="/" className="hover:text-[#00ADB5] w-full text-center py-2 hover:bg-[#172b36]">
-            Home
-          </Link>
-          <Link href="/downloadApp" className="hover:text-[#00ADB5] w-full text-center py-2 hover:bg-[#172b36]">
-            Download App
-          </Link>
-          <Link href="/contactus" className="hover:text-[#00ADB5] w-full text-center py-2 hover:bg-[#172b36] mb-3">
-            Contact Us
-          </Link>
-          <Link href="/register" className="bg-[#00ADB5] hover:bg-[#008C96] px-4 py-2.5 rounded-xl text-sm font-medium">
+          {navLinks.map((link) => (
+            <Link key={link.path} href={link.path} className={`hover:text-[#00ADB5] w-full text-center py-2 hover:bg-[#172b36] ${pathname === link.path ? "text-[#00ADB5] bg-[#172b36]" : ""}`}>
+              {link.name}
+            </Link>
+          ))}
+          <Link href="/register" className="bg-[#00ADB5] hover:bg-[#008C96] px-4 py-2.5 rounded-xl text-sm font-medium mt-2">
             Sign Up
           </Link>
         </div>
